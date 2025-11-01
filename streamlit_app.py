432ebc857f713d2391aca4403a132441a2fd9426
Jean-Michel DOUAMPO <118279295+jdouampo@users.noreply.github.com>
2025-11-01 17:22:00 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,13 +36,17 @@ try:
         fruits_list,
         max_selections=5
     )
-
-    if ingredients_list and name_on_order:
-        ingredients_string = ' '.join(ingredients_list)
-        for fruit_chosen in ingredients_string:
-            st.subheader(fruit_chosen+ " Nutrition information")
-            smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon"+fruit_chosen)
-            sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+        if ingredients_list:
+            ingredients_string =''
+            for fruit_chosen in ingredients_list:
+                ingredients_string+=fruit_chosen + ' '
+                st.subheader(fruit_chosen+ " Nutrition information")
+                smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon"+fruit_chosen)
+                sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+    #if ingredients_list and name_on_order:
+       # ingredients_string = ' '.join(ingredients_list)
+        #for fruit_chosen in ingredients_string:
+            
         
         if st.button('Submit Order'):
             # Méthode avec SQL parameterisé (plus sécurisée)
