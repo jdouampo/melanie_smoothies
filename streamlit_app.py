d8ac104bd558f3158dcfe25e00b6a3093d167c57
Jean-Michel DOUAMPO <118279295+jdouampo@users.noreply.github.com>
2025-11-01 14:52:16 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 
-cnx =st.connection("snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
